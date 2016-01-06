827f74581d64eb7be665167d9e8136667064075c
Remus Lazar <remus@lazar.info>
2016-01-06 18:23:33 +0100

    Initial Commit

---
+++ xliff-tool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xliff-tool
+//
+//  Created by Remus Lazar on 06.01.16.
+//  Copyright © 2016 Remus Lazar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xliff-tool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xliff-tool
+//
+//  Created by Remus Lazar on 06.01.16.
+//  Copyright © 2016 Remus Lazar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
